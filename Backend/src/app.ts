b6c277e83b1ec57efbfb5db41094840ef535e6c8
shivanshu877 <vashudev8777@gmail.com>
2024-06-09 21:25:29 +0530

    route changed

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -13,7 +13,9 @@ app.use(morgan("dev")); //It can be used to log requests, errors, and more to th
 app.use(express.json()); //It can be used to parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/tweets", tweetRoutes);
 app.use("/api/follow", followRoutes);
