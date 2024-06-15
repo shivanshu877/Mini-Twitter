21941867a8703396ed76572f4da80d45ea5dc7bd
Rudrax1000 <112767288+Rudrax1000@users.noreply.github.com>
2024-06-15 10:26:32 +0530

    signupform  validation fix

--- Frontend/src/pages/SignupForm.jsx
+++ Frontend/src/pages/SignupForm.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -8,32 +7,39 @@ import Loading from "./Loading";
 const SignupForm = ({ switchToLogin }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    let trimmedUsername = username.trim();
 
-    // Validation
-    if (username.length < 4) {
+    // Check if username contains spaces
+    if (/\s/.test(trimmedUsername)) {
+      toast.error("Username must not contain spaces.");
+      return;
+    }
+    // Check if username is at least 4 characters long
+    if (trimmedUsername.length < 4) {
       toast.error("Username must be at least 4 characters long.");
       return;
     }
 
+    // Check if password is at least 6 characters long
     if (password.length < 6) {
       toast.error("Password must be at least 6 characters long.");
       return;
     }
 
-    setLoading(true); 
+    setLoading(true);
 
     try {
       const response = await auth.signup(username, password);
-      toast.success("User registered successfully!"); 
+      toast.success("User registered successfully!");
       switchToLogin();
     } catch (error) {
       toast.error(error.message);
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
@@ -64,7 +70,6 @@ const SignupForm = ({ switchToLogin }) => {
         disabled={loading}
       >
         {loading ? <Loading /> : "Signup"}{" "}
-        {/* Conditionally render the loading component */}
       </button>
       <p className="mt-3 text-center">
         Already have an account?{" "}
